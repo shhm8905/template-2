64c98fb2e4d1791ed8a81e4305ff0412c8bc68d4
Hazem Khalil <hazem-khalil@hotmail.com>
2021-04-14 18:29:22 +0200

    Upload new version

--- demo.js
+++ demo.js
@@ -2,7 +2,7 @@ window.onload = () => {
 
 
     //  Add default local storage class on body
-    document.body.classList.add(localStorage.getItem("pageColor") || '.red');
+    document.body.classList.add(localStorage.getItem("pageColor") || '.black');
 
     var list = document.querySelectorAll(".color-switcher li");
     var classesList = [];
@@ -29,17 +29,26 @@ window.onload = () => {
 
     var inpt = document.querySelector('.inpt'),
         sendBtn = document.querySelector('.send-btn'),
-        printText = document.querySelector('.print-text');
-
-    printText.textContent = localStorage.getItem('resultItem');
+        printText = document.querySelector('.print-text'),
+        result = localStorage.getItem('resultItem');
+        if(result){
+            printText.textContent=result;
+        }
+        
+    inpt.addEventListener("change",()=>{
+        localStorage.removeItem("resultItem");
+        printText.textContent=""
+    })
+    
 
     sendBtn.addEventListener("click", () => {
         if (inpt.value >= 2 && inpt.value <= 100) {
             var myRusalt = inpt.value / 1.6 * 22 * 12 * 9.5;
-
             localStorage.setItem("resultItem", printText.textContent = `${inpt.value} km = ${myRusalt} SEK => This is the price throughout the year.`);
+            inpt.value="";
         } else if (inpt.value > 100 || inpt.value < 2) {
             printText.textContent = "Please enter number but not more than 100 km and not less than 2 km";
+            inpt.value="";
         }
     })
 
